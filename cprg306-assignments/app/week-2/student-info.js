fdc15b23f23e72a8410aef27b6ffca787e98f1fb
Harshit Khurana <harshitkhurana@Harshits-MacBook-Pro.local>
2024-01-15 14:39:57 -0700

    All files are added in my project

---
+++ cprg306-assignments/app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function StudentInfo() {
+    return (
+      <main>
+        <p>Harshit Khurana</p>
+        <p><Link href="https://github.com/harshitkhuranasait">Link to my profile</Link></p>
+      </main>
+    );
+  }
