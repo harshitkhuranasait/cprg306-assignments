d84d8a22b9a572c286bfa3d4d7635e0748e47951
harshitkhuranasait <131495319+harshitkhuranasait@users.noreply.github.com>
2024-02-06 11:14:01 -0700

    Week3 Assignment

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
-import Image from 'next/image'
-import Link from 'next/link'
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1> This is an update for the weekly work done</h1>
-      <Link href = "./week-2"> Hello </Link>
-      </main>
-     
-  )
-}
+import Link from "next/link";
+ 
+ export default function Page() {
+  return(
+    <main className="container mx-auto p-4 bg-grey-200 min-h-screen items-center justify-center flex-col">
+      <h1 className="text-4xl front-extrabold text-blue-500 mb-8">CPRG-306: Web Development 2</h1>
+      <div className="flex justify-center items-center"></div>
+      <Link href="week-2" className="text-blue-500 hover:text-green-500 transition duration-500"> Assignments week-2</Link>
+ 
+      <div className="flex justify-center items-center"></div>
+      <Link href="week-3" className="text-blue-500 hover:text-green-500 transition duration-500"> Assignments week-3</Link>
+    </main>
+  );
+};
---
+++ app/week-3/page.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ItemList from './item-list';
+ 
+const Page = () => {
+  return (
+    <main className="bg-red-100 p-5 min-h-screen">
+      <div className="max-w-2xl mx-auto">
+        <h1 className="text-2xl  text-blue-900 mb-4">Shopping List</h1>
+        <ul className="bg-blue-300  p-5">
+          {ItemList.map((item, index) => (
+            <li key={index} className="border-b py-2 ">
+              <div className="text-grey-600">{item.name}</div>
+              <div className="text-grey-600">Quantity : {item.quantity}</div>
+              <div className="text-grey-600">Category : {item.category}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
+};
+ 
+export default Page;
+ 
